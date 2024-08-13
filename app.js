0d6071e1637b3e27be267d3f357f35369b6d05a9
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2024-08-13 19:58:57 +0100

    Create a 16x16 grid of divs

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hi Mom!");
+const GRID_SIZE = 16 * 16;
+const container = document.querySelector(".container");
+
+for (let i = 0; i < GRID_SIZE; i++) {
+  const div = document.createElement("div");
+  container.appendChild(div);
+}
