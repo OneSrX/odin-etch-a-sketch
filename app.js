369f566741e1836dc7d653c0be4e8cca506f661b
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2024-08-13 23:50:25 +0100

    Handle new grid creation
    the new grid will be generated in the same width as before

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
+const PAD_WIDTH = 560;
 
-button.addEventListener("click", () => {
-  let userInput = parseInt(prompt("Enter your preferred grid size: "));
-  container.innerHTML = "";
-  drawGrid(userInput);
-});
+// set the default width of the divs container
+container.style.width = `${PAD_WIDTH}px`;
 
 function drawGrid(size = 16) {
   for (let i = 0; i < size ** 2; i++) {
@@ -15,8 +13,29 @@ function drawGrid(size = 16) {
 
   const divs = document.querySelectorAll(".container > div");
   divs.forEach((div) => {
+    div.style.cssText = `
+      width: calc(${PAD_WIDTH}px / ${size});
+      aspect-ratio: 1/1;
+      border: 1px solid #eee;
+    `;
+
     div.addEventListener("mouseenter", () => {
       div.style.backgroundColor = "red";
     });
   });
 }
+
+// draw the grid with default size
+drawGrid();
+
+button.addEventListener("click", () => {
+  let userInput = parseInt(prompt("Enter your preferred grid size: "));
+
+  if (isNaN(userInput) || userInput <= 0 || userInput > 100) {
+    alert("Please enter a number between 1 and 100");
+    return;
+  }
+
+  container.innerHTML = ""; // clear existing grid
+  drawGrid(userInput); // re-draw the grid with the new size
+});
